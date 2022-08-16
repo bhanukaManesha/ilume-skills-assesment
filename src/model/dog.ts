1b99596e536916a5e61629b26b54bebda38168d4
Bhanuka Gamage <bhanukamgamage@gmail.com>
2022-08-17 08:03:45 +1000

    Update DogSchema with owner and index

--- src/model/dog.ts
+++ src/model/dog.ts
@@ -4,7 +4,10 @@ const DogSchema = new mongoose.Schema({
   name: { type: String, },
   dob: { type: Date},
   breed: {type: mongoose.Schema.Types.ObjectId, ref: 'Breed'},
+  owner: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
 });
+
+DogSchema.index({ name: 1, owner: 1 }, { unique: true });
 
 const Dog = mongoose.model('Dog', DogSchema);
 export default Dog;
